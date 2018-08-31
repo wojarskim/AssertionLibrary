16488e80b463f3a3b36f1c89391af639088f2b0a
wojarskim <wojarskim@gmail.com>
2018-08-31 10:42:11 +0200

    Simple assertion library for Applandeo

---
+++ AssertionLibrary.Tests/UnitTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssertionLibrary;
+
+namespace Unit.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Test_Eq()
+        {
+            6.Expect().Eq(6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExpectationFailedExceptin))]
+        public void Test_Eq_Fails()
+        {
+            6.Expect().Eq(5);
+        }
+
+        [TestMethod]
+        public void Test_Greater()
+        {
+            10.Expect().IsGreater(6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExpectationFailedExceptin))]
+        public void Test_Greater_Fails()
+        {
+            (-10).Expect().IsGreater(4);
+        }
+
+        [TestMethod]
+        public void Test_NotEq()
+        {
+            10.Expect().Not().Eq(9);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExpectationFailedExceptin))]
+        public void Test_NotEq_Fails()
+        {
+            10.Expect().Not().Eq(10);
+        }
+
+        [TestMethod]
+        public void Test_Raise_Error()
+        {
+            (new Action(() => { throw new Exception(); })).Expect().RaiseError();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExpectationFailedExceptin))]
+        public void Test_Raise_Error_Fails()
+        {
+            (new Action(() => FizBar.SampleMethod())).Expect().RaiseError();
+        }
+
+        [TestMethod]
+        public void Test_Properties()
+        {
+            var tested = new FizBar() { Bar = "Bar", Fiz = String.Empty };
+            var expected = new { Bar = "Bar", Fiz = String.Empty };
+            tested.Expect().Properties().Eq(expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExpectationFailedExceptin))]
+        public void Test_Properties_Fails()
+        {
+            var tested = new FizBar() { Bar = "Bar", Fiz = String.Empty };
+            var expected = new { Bar = "Bar2", Fiz = String.Empty };
+            tested.Expect().Properties().Eq(expected);
+        }
+
+        [TestMethod]
+        public void Test_PropertiesWithout()
+        {
+            var tested = new FizBar() { Bar = "Bar", Fiz = "Fiz" };
+            var expected = new { Bar = "Bar", Fiz = "Fiz2" };
+            tested.Expect().PropertiesWithout(x => x.Fiz).Eq(expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExpectationFailedExceptin))]
+        public void Test_PropertiesWithout_Fails()
+        {
+            var tested = new FizBar() { Bar = "Bar", Fiz = "Fiz" };
+            var expected = new { Bar = "Bar2", Fiz = "Fiz2" };
+            tested.Expect().PropertiesWithout(x => x.Fiz).Eq(expected);
+        }
+    }
+
+    public class FizBar
+    {
+        public string Bar { get; set; }
+        public string Fiz { get; set; }
+
+        public static int SampleMethod()
+        {
+            return 2 + 2;
+        }
+    }
+}
---
+++ AssertionLibrary/AssertionLibrary.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace AssertionLibrary
+{
+    public class ExpectationFailedExceptin : Exception { }
+
+    public static class AssertionLibrary
+    {
+        public static Action<Predicate<TSubject>> Expect<TSubject>(this TSubject subject)
+        {
+            return (Predicate<TSubject> predicate) =>
+            {
+                if (!predicate.Invoke(subject))
+                {
+                    throw new ExpectationFailedExceptin();
+                }
+            };
+        }
+
+        public static void Eq<TSubject>(this Action<Predicate<TSubject>> assertion, object other)
+        {
+            assertion.Invoke((subject) =>
+            {
+                return subject.Equals(other);
+            });
+        }
+
+        public static void IsGreater<TSubject>(this Action<Predicate<TSubject>> assertion, TSubject other) where TSubject : IComparable
+        {
+            assertion.Invoke((subject) =>
+            {
+                return subject.CompareTo(other) > 0;
+            });
+        }
+
+        public static Action<Predicate<TSubject>> Not<TSubject>(this Action<Predicate<TSubject>> assertion)
+        {
+            return (Predicate<TSubject> predicate) =>
+            {
+                assertion.Invoke((subject) =>
+                {
+                    return !predicate.Invoke(subject);
+                });
+            };
+        }
+
+        public static void RaiseError<TSubject>(this Action<Predicate<TSubject>> assertion) 
+        {
+            assertion.Invoke((subject) =>
+            {
+                try
+                {
+                    (subject as Action)?.Invoke();
+                }
+                catch
+                {
+                    return true;
+                }
+                return false;
+            });
+        }
+
+        public static Action<Predicate<AssertionSubjectProperties>> Properties<TSubject>(this Action<Predicate<TSubject>> assertion)
+        {
+            return (Predicate<AssertionSubjectProperties> predicate) =>
+            {
+                assertion.Invoke((subject) =>
+                {
+                    var newSubject = new AssertionSubjectProperties(subject);
+                    return predicate.Invoke(newSubject);
+                });
+            };
+        }
+
+        public static Action<Predicate<AssertionSubjectProperties>> PropertiesWithout<TSubject, TResult>(this Action<Predicate<TSubject>> assertion, Expression<Func<TSubject, TResult>> excluder)
+        {
+            return (Predicate<AssertionSubjectProperties> predicate) =>
+            {
+                assertion.Invoke((subject) =>
+                {
+                    var excludedPropertyName = (excluder.Body as MemberExpression)?.Member?.Name;
+                    var newSubject = new AssertionSubjectProperties(subject, excludedPropertyName);
+                    return predicate.Invoke(newSubject);
+                });
+            };
+        }
+    }
+}
